861783438f1587ab2a2453c6e4086dd0ae08782d
Amir xajeh <axajeh@gmail.com>
2022-07-29 20:19:59 +0430

    [UPDATE]: todoCreateDto

--- server/src/todo/dto/create-todo.dto.ts
+++
@@ -1,6 +0,0 @@
-import { Prisma } from "@prisma/client";
-
-export class CreateTodoDto implements Prisma.TodoCreateArgs {
-  select?: Prisma.TodoSelect;
-  data: (Prisma.Without<Prisma.TodoCreateInput, Prisma.TodoUncheckedCreateInput> & Prisma.TodoUncheckedCreateInput) | (Prisma.Without<Prisma.TodoUncheckedCreateInput, Prisma.TodoCreateInput> & Prisma.TodoCreateInput);
-}
